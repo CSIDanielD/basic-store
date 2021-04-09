707ab1699804af38f2fa2f66c8a2111bc240f897
Daniel Durbin <Daniel.Durbin@csiweb.com>
2021-04-09 11:21:30 -0500

    added provider service functionality

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,9 +1,75 @@
 import { Injectable } from '@angular/core';
+import { withState } from '../basic-store/actionContext';
+import { AppState } from '../types/appState';
+import { Note } from '../types/note';
+import { FakeBackendService } from './fake-backend.service';
+
+// Create a handy context to easily create strongly-type reducer functions
+const context = withState<AppState>();
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
+  constructor(private fakeBackend: FakeBackendService) { }
 
-  constructor() { }
+  getNotesAsync = context.createReducer.withoutPayload(async getState => {
+    // Fetch & await data from the fake API
+    const notes = await this.fakeBackend.getNotes().toPromise();
+
+    // Get the current state after awaiting
+    const state = getState();
+    state.notes = notes;
+    return state;
+  });
+
+  addNoteAsync = context.createReducer.withPayload(async (getState, note: Note) => {
+    // Fetch & await data from the fake API
+    const result = await this.fakeBackend.addNote(note).toPromise();
+
+    // Get the current state after awaiting
+    const state = getState();
+
+    if (result) {
+      state.notes.push(note);
+    }
+
+    return state;
+  });
+
+  updateNoteAsync = context.createReducer.withPayload(
+    async (getState, payload: { noteId: number; note: Note }) => {
+      // Fetch & await data from the fake API
+      const result = await this.fakeBackend
+        .updateNote(payload.noteId, payload.note)
+        .toPromise();
+
+      // Get the current state after awaiting
+      const state = getState();
+
+      if (result) {
+        const foundIndex = state.notes.findIndex(
+          n => n.noteId == payload.noteId
+        );
+        state.notes.splice(foundIndex, 1, payload.note); // Replace the note in the state with the new one.
+      }
+
+      return state;
+    }
+  );
+
+  removeNoteAsync = context.createReducer.withPayload(async (getState, noteId: number) => {
+    // Fetch & await data from the fake API
+    const result = await this.fakeBackend.removeNote(noteId).toPromise();
+
+    // Get the current state after awaiting
+    const state = getState();
+
+    if (result) {
+      const foundIndex = state.notes.findIndex(n => n.noteId == noteId);
+      state.notes.splice(foundIndex, 1); // Delete the found note.
+    }
+
+    return state;
+  });
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { withState } from '../basic-store/actionContext';
+import { AppState } from '../types/appState';
+import { UserTask } from '../types/userTask';
+import { FakeBackendService } from './fake-backend.service';
+
+// Create a handy context to easily create strongly-type reducer functions
+const context = withState<AppState>();
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  constructor(private fakeBackend: FakeBackendService) { }
 
-  constructor() { }
+  addTaskAsync = context.createReducer.withPayload(async (getState, task: UserTask) => {
+    // Fetch & await data from the fake API
+    const result = await this.fakeBackend.addTask(task).toPromise();
+
+    // Get the current state after awaiting
+    const state = getState();
+
+    if (result) {
+      state.tasks.push(task);
+    }
+
+    return state;
+  });
+
+  updateTaskAsync = context.createReducer.withPayload(
+    async (getState, payload: { taskId: number; task: UserTask }) => {
+      // Fetch & await data from the fake API
+      const result = await this.fakeBackend
+        .updateTask(payload.taskId, payload.task)
+        .toPromise();
+
+      // Get the current state after awaiting
+      const state = getState();
+
+      if (result) {
+        const foundIndex = state.tasks.findIndex(
+          t => t.taskId === payload.taskId
+        );
+
+        state.tasks.splice(foundIndex, 1, payload.task);
+      }
+
+      return state;
+    }
+  );
+
+  removeTaskAsync = context.createReducer.withPayload(async (getState, taskId: number) => {
+    // Fetch & await data from the fake API
+    const result = await this.fakeBackend.removeTask(taskId).toPromise();
+
+    // Get the current state after awaiting
+    const state = getState();
+
+    if (result) {
+      const foundIndex = state.tasks.findIndex(t => t.taskId === taskId);
+
+      state.tasks.splice(foundIndex, 1);
+    }
+
+    return state;
+  });
 }
