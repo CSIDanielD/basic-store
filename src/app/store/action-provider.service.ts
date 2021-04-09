c2eee5a1be49f929b128ee8a81ebad48aa66f516
Daniel Durbin <Daniel.Durbin@csiweb.com>
2021-04-09 11:43:50 -0500

    added actions to actionProvider

--- src/app/store/action-provider.service.ts
+++ src/app/store/action-provider.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@angular/core';
 import { createProviderFrom } from '../basic-store/actionProvider';
+import { NoteService } from '../services/note.service';
+import { TaskService } from '../services/task.service';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActionProviderService {
+  /** 
+ * These are all the actions that are provided to the store. To add a new provider, 
+ * inject it into this service and add a .mergeProvider({...this.your_service}) here. 
+ * You will need to use the spread operator syntax (see link) here for your provider's actions to be correctly merged.
+ * 
+ * Also note that that your action provider's public properties can ONLY be Reducer methods. You *are allowed* to have
+ * other properties on the object, but they have to have the `protected` or `private` modifier.
+ * 
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#spread_in_object_literals
+ * */
+  private _mergedActionProviders = createProviderFrom({ ...this.userService })
+    .mergeProvider({ ...this.taskService })
+    .mergeProvider({ ...this.noteService });
 
-  provider = createProviderFrom({}).provider;
 
-  constructor() { }
+  provider = this._mergedActionProviders.provider;
+
+  constructor(
+    private userService: UserService,
+    private taskService: TaskService,
+    private noteService: NoteService) { }
 }
