9c77cb40ebd02f15e7f5b3da515426cb319e0c61
Daniel Durbin <Daniel.Durbin@csiweb.com>
2021-04-09 10:56:14 -0500

    added list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { NoteListComponent } from './components/note-list/note-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    TaskListComponent,
+    NoteListComponent
   ],
   imports: [
     BrowserModule
